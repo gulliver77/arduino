0c65b464ca0b90f6a1c698c1d5843d6e570135b0
gulliver77 <contact@gulliver77.org>
2022-05-12 16:32:14 +0200

    version 12-05-2022

--- 6servos/6servos.ino
+++ 6servos/6servos.ino
@@ -1,5 +1,5 @@
 /*
-12-05-2022
+13-05-2022
 TODO :
 remplacer par min et max
 -------
@@ -10,7 +10,6 @@ poignet
 avantbras
 bras
 base
-
 attach PWM des servos
   servobase.attach(3);  
   servobras.attach(5);  
@@ -18,6 +17,15 @@ attach PWM des servos
   servopoignet.attach(9);   
   servomain.attach(10);
   servopince.attach(11);
+
+occupation des pattes arduino
+                  serie 1/2
+                    i2c A4/A5
+        capteur couleur 7/8/12
+                 libres 13
+                     BP 4
+  pb appuyé au démarrage => potentiometre sinon automatique
+  effet de bord tremblottement
 */
 
 #include <Servo.h> 
@@ -31,27 +39,40 @@ attach PWM des servos
 #define potpince A7 
 #define delaisAction 20
 #define delaiSetup 20
+#define bp 4
 
 // import AB (7-04-2022)
 int inbase, inbras, inavantbras, inpoignet, inmain, inpince ;
 int posbase, posbras, posavantbras, pospoignet, posmain, pospince ;
 int consignebase, consignebras, consigneavantbras, consignepoignet, consignemain, consignepince ;
 
+/*
+position  base bras  av bras  poignet  main  pince  
+attrape   0     15    44        28      94
+elevation       38
+lecteur   87    45    78        20      94
+vert      141   0     9         45      94
+bleu      165   30    51        45      94
+rouge     180   36    44        28      94
+
+*/
 
 // tableau min/max/init des servomoteurs
-int minbase=0,        maxbase=180,        initbase=82,
-    minbras=0,       maxbras=135,        initbras=1,
-    minavantbras=45,  maxavantbras=135,   initavantbras=85,
-    minpoignet=45,    maxpoignet=135,     initpoignet=86,
-    minmain=45,       maxmain=135,        initmain=88,
+int minbase=0,        maxbase=180,        initbase=87,
+    minbras=0,       maxbras=135,        initbras=57,
+    minavantbras=5,  maxavantbras=135,   initavantbras=103,
+    minpoignet=3,    maxpoignet=135,     initpoignet=20,
+    minmain=45,       maxmain=135,        initmain=90,
     minpince=100,     maxpince=155,       initpince=130;
 
-
+boolean mode=1;
  
 Servo servobase, servobras, servoavantbras, servopoignet, servomain, servopince;  // create servo objects to control a servo 
  
 void setup() {
   Serial.begin(9600);
+  pinMode(bp,INPUT_PULLUP);
+  mode=digitalRead(bp);  
   servobase.attach(3); 
   servobase.write(initbase);
   delay(delaiSetup); 
@@ -69,18 +90,44 @@ void setup() {
   delay(delaiSetup);
   servopince.attach(11);
   servopince.write(initpince);
-  
+  Serial.print(" mode : " + String(mode));
+  delay(5000);
 }
  
 void loop() {
- 
+ if(mode) demo(); else potards();
+}
+
+void affdebug()
+{
+// controle affichage entrée potentiomètre, sortie valeur map 
+//  Serial.println("----------");
+ Serial.print(" base : " + String(posbase));
+  Serial.print(" bras : " + String(posbras));
+  Serial.print(" avantbras  : "  + String(posavantbras));
+  Serial.print(" poignet : " + String(pospoignet));
+  Serial.print(" main : " + String(posmain)); 
+  Serial.println(" pince : " + String(pospince));
+}
+
+void demo()
+{
+    Serial.println("mode démo");
+  delay(1000);
+  if (!digitalRead(bp)) prendre();  
+}
+
+void potards()
+{
+  Serial.print("mode potards");
+
 //gestion base (AB 7-04-2022)
-  consignebase = map(analogRead(potbase), 0, 1023, minbase, maxbase); //lecture du potentiomètre de la base et transformation en degrés entre minbase et maxbase
+  consignebase = map(analogRead(potbase), 1023, 0, minbase, maxbase); //lecture du potentiomètre de la base et transformation en degrés entre minbase et maxbase
   posbase = servobase.read();//lecture de la position du servo de la base
   if (posbase  < consignebase)servobase.write(posbase +1);else if(posbase  > consignebase) servobase.write(posbase -1); 
 
 //gestion bras
-  consignebras = map(analogRead(potbras), 0, 1023, minbras, maxbras);
+  consignebras = map(analogRead(potbras), 1023, 0, minbras, maxbras);
   posbras = servobras.read();
   if (posbras  < consignebras)servobras.write(posbras +1);else if(posbras  > consignebras) servobras.write(posbras -1); 
 
@@ -90,7 +137,7 @@ void loop() {
   if (posavantbras  < consigneavantbras)servoavantbras.write(posavantbras +1);else if(posavantbras  > consigneavantbras) servoavantbras.write(posavantbras -1); 
 
  //gestion poignet  
-  consignepoignet = map(analogRead(potpoignet), 0, 1023, minpoignet, maxpoignet);
+  consignepoignet = map(analogRead(potpoignet), 1023, 0, minpoignet, maxpoignet);
   pospoignet = servopoignet.read();
   if (pospoignet  < consignepoignet)servopoignet.write(pospoignet +1);else if(pospoignet  > consignepoignet) servopoignet.write(pospoignet -1); 
 
@@ -104,18 +151,23 @@ void loop() {
   pospince = servopince.read();
   if (pospince  < consignepince)servopince.write(pospince +1);else if(pospince  > consignepince) servopince.write(pospince -1); 
 
-  affdebug();
-
+  affdebug(); 
 }
 
-void affdebug()
+void prendre()
 {
-// controle affichage entrée potentiomètre, sortie valeur map 
-//  Serial.println("----------");
- Serial.print(" base : " + String(posbase));
-  Serial.print(" bras : " + String(posbras));
-  Serial.print(" avantbras  : "  + String(posavantbras));
-  Serial.print(" poignet : " + String(pospoignet));
-  Serial.print(" main : " + String(posmain)); 
-  Serial.println(" pince : " + String(pospince));
+/*  position  base bras  av bras  poignet  main  pince*/ 
+int prendre[6]={0,  15,    44,      28,     94, 155};
+
+posbras = servobras.read();
+posavantbras = servoavantbras.read();
+/*if (posbras  < prendre[1])*/
+for (int i=1; i<=20; i++)
+{
+  servobras.write(posbras+i);
+  servoavantbras.write(posavantbras-i);  
+  delay(100);
+}
+
+delay(5000);
 }
